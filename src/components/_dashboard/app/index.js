8a47f46ade263dceb600e4b728abf392ed5d03fb
Miguel Tejedor <m.tejedor@medlabmg.com>
2021-11-08 17:43:25 +0100

    ♻️ fix npm install, refactor: delete products and blog

--- src/components/_dashboard/app/index.js
+++ src/components/_dashboard/app/index.js
@@ -1,7 +1,6 @@
 export { default as TopPerformers } from './TopPerformers';
 export { default as AppCurrentSubject } from './AppCurrentSubject';
 export { default as PositionsRepartition } from './PositionsRepartition';
-export { default as AppNewsUpdate } from './AppNewsUpdate';
 export { default as LastOrders } from './LastOrders';
 export { default as AppTasks } from './AppTasks';
 export { default as AppTrafficBySite } from './AppTrafficBySite';
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,7 @@ import { Navigate, useRoutes } from 'react-router-dom';
 // layouts
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
-//
 import DashboardApp from './pages/DashboardApp';
-// import Products from './pages/Products';
-// import Blog from './pages/Blog';
 import OrderHistory from './pages/OrderHistory';
 import NotFound from './pages/Page404';
 
@@ -24,8 +21,6 @@ export default function Router() {
           element: <DashboardApp />
         },
         { path: 'orderHistory', element: <OrderHistory /> }
-        // { path: 'products', element: <Products /> },
-        // { path: 'blog', element: <Blog /> }
       ]
     },
     {
@@ -33,7 +28,7 @@ export default function Router() {
       element: <LogoOnlyLayout />,
       children: [
         { path: '404', element: <NotFound /> },
-        { path: '/', element: <Navigate to="/dashboard" /> },
+        { path: '/', element: <Navigate to="/dashboard/app" /> },
         { path: '*', element: <Navigate to="/404" /> }
       ]
     },
